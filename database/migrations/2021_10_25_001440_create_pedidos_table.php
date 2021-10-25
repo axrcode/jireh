240f5c9d7d3bac7ea1e5f77dc751943f628d8188
axrcode <axrcode@gmail.com>
2021-10-24 18:24:43 -0600

    Add - Migraciones para pedidos y clientes

---
+++ database/migrations/2021_10_25_001440_create_pedidos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->text('descripcion')->nullable();
+            $table->string('fecha_pedido')->nullable();
+            $table->string('fecha_entrega')->nullable();
+            $table->string('estado')->default('pendiente');
+            $table->foreignId('empleado_id')->references('id')->on('empleados');
+            $table->foreignId('cliente_id')->references('id')->on('clientes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+}
