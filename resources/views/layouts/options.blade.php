dbdb1a73e722f33206f6b1a416945cf8b37df02d
axrcode <axrcode@gmail.com>
2021-10-24 23:46:40 -0600

    Update - Opciones en drawer

--- resources/views/layouts/options.blade.php
+++ resources/views/layouts/options.blade.php
@@ -1,6 +1,27 @@
 <!-- Opciones de Administrador -->
 
     <li class="nav-item">
+        <a href="{{ route('dashboard') }}" class="nav-link">
+            <i class="nav-icon fas fa-tachometer-alt"></i>
+            <p>Tablero</p>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a href="{{ route('admin.pedidos.index') }}" class="nav-link">
+            <i class="nav-icon fas fa-users"></i>
+            <p>Clientes</p>
+        </a>
+    </li>
+
+    <li class="nav-item">
+        <a href="{{ route('admin.pedidos.index') }}" class="nav-link">
+            <i class="nav-icon fas fa-cart-arrow-down"></i>
+            <p>Pedidos</p>
+        </a>
+    </li>
+
+    <li class="nav-item">
         <a href="#" class="nav-link">
             <i class="fas fa-cogs nav-icon"></i>
             <p>
@@ -17,30 +38,23 @@
                         <p>{{ __('General Parameters') }}</p>
                     </a>
                 </li>
+
+                <li class="nav-item">
+                    <a href="{{ route('admin.configuracion.generales') }}" class="nav-link">
+                        <i class="fas fa-user nav-icon"></i>
+                        <p>Empleados</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="{{ route('admin.configuracion.generales') }}" class="nav-link">
+                        <i class="fas fa-sitemap nav-icon"></i>
+                        <p>Departamentos</p>
+                    </a>
+                </li>
             </ul>
 
     </li>
-
-
-
-    <li class="nav-item">
-        <a href="{{ route('dashboard') }}" class="nav-link">
-            <i class="nav-icon fas fa-tachometer-alt"></i>
-            <p>
-                {{ __('Dashboard') }}
-            </p>
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a href="{{ route('admin.pedidos.index') }}" class="nav-link">
-            <i class="nav-icon fas fa-cart-arrow-down"></i>
-            <p>
-                Pedidos
-            </p>
-        </a>
-    </li>
-
 
     {{-- <li class="nav-item">
         <a href="{{ route('admin.colaborador.index') }}" class="nav-link">
