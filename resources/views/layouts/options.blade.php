df2ac0742e1239c14da65c8f08910db08729d32f
axrcode <axrcode@gmail.com>
2021-10-31 21:17:34 -0600

    Add - permisos en vista drawer

--- resources/views/layouts/options.blade.php
+++ resources/views/layouts/options.blade.php
@@ -1,39 +1,49 @@
 <!-- Opciones de Administrador -->
 
+    @can('admin/dashboard')
     <li class="nav-item">
         <a href="{{ route('dashboard') }}" class="nav-link">
             <i class="nav-icon fas fa-tachometer-alt"></i>
             <p>Tablero</p>
         </a>
     </li>
+    @endcan
 
+    @can('admin/departamentos')
     <li class="nav-item">
         <a href="{{ route('admin.departamentos.index') }}" class="nav-link">
             <i class="nav-icon fas fa-sitemap nav-icon"></i>
             <p>Departamentos</p>
         </a>
     </li>
+    @endcan
 
+    @can('admin/empleados')
     <li class="nav-item">
         <a href="{{ route('admin.empleados.index') }}" class="nav-link">
             <i class="nav-icon fas fa-user nav-icon"></i>
             <p>Empleados</p>
         </a>
     </li>
+    @endcan
 
+    @can('admin/clientes')
     <li class="nav-item">
         <a href="{{ route('admin.clientes.index') }}" class="nav-link">
             <i class="nav-icon fas fa-users"></i>
             <p>Clientes</p>
         </a>
     </li>
+    @endcan
 
+    @can('admin/pedidos')
     <li class="nav-item">
         <a href="{{ route('admin.pedidos.index') }}" class="nav-link">
             <i class="nav-icon fas fa-cart-arrow-down"></i>
             <p>Pedidos</p>
         </a>
     </li>
+    @endcan
 
     {{-- <li class="nav-item">
         <a href="#" class="nav-link">
